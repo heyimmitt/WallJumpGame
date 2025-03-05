1b9fda666bcf171707728e9b12164b52f06025f7
heyimmitt <anoushka.basak03@gmail.com>
2025-03-06 01:38:24 +0530

    player can now jump, next to make sure they can only jump wall to wall

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class playerScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
+    public Rigidbody2D rb;
+    public float jumpForce = 5f;
+    public bool isOnLeftWall = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // gets reference to rb
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        if (Input.GetKeyDown(KeyCode.Space) == true) {
+            if (isOnLeftWall) { //jumping and switching of walls 
+                rb.linearVelocity = new Vector2(jumpForce, jumpForce);
+                isOnLeftWall = false;
+            }
+            else {
+                rb.linearVelocity = new Vector2(-jumpForce, jumpForce);
+                isOnLeftWall = true;
+            }
+        }
     }
 }
