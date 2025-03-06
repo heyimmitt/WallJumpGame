72d9f015109e6f08ad2c095d0b243e1113a3a703
heyimmitt <anoushka.basak03@gmail.com>
2025-03-06 21:36:23 +0530

    walls get destroyed once they are out of frame now

--- Assets/wallScript.cs
+++ Assets/wallScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class wallScript : MonoBehaviour
 {
     public float moveDownSpeed = 2;
-
+    public float deadZone = -7;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +14,10 @@ public class wallScript : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3.down * moveDownSpeed) * Time.deltaTime; //moves the wall downward at a fixed speed
+
+        if (transform.position.y < deadZone)
+        {
+            Destroy(gameObject); 
+        }
     }
 }
